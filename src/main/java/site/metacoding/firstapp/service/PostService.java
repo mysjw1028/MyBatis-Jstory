355bfa82cbc67f038fd7f9393ae782da46adf95f
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2023-02-01 12:06:33 +0900

    구독 xml 기능 작성 완료

--- src/main/java/site/metacoding/firstapp/service/PostService.java
+++ src/main/java/site/metacoding/firstapp/service/PostService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.firstapp.domain.love.Love;
 import site.metacoding.firstapp.domain.love.LoveDao;
 import site.metacoding.firstapp.domain.post.PostDao;
+import site.metacoding.firstapp.domain.subscribe.Subscribe;
+import site.metacoding.firstapp.domain.subscribe.SubscribeDao;
 import site.metacoding.firstapp.web.dto.post.PostDatailDto;
 
 @RequiredArgsConstructor
@@ -14,9 +16,10 @@ import site.metacoding.firstapp.web.dto.post.PostDatailDto;
 public class PostService {
     private final PostDao postDao;
     private final LoveDao loveDao;
+    private final SubscribeDao subscribeDao;
 
-    public void 좋아요취소(Integer loveId) {
-        loveDao.deleteById(loveId);
+    public PostDatailDto 게시글상세보기(@PathVariable Integer postId, @PathVariable Integer userId) {
+        return postDao.findByDetail(postId, userId);
     }
 
     public Love 좋아요(Love love) {
@@ -24,7 +27,17 @@ public class PostService {
         return love;
     }
 
-    public PostDatailDto 게시글상세보기(@PathVariable Integer postId, @PathVariable Integer userId) {
-        return postDao.findByDetail(postId, userId);
+    public void 좋아요취소(Integer loveId) {
+        loveDao.deleteById(loveId);
     }
+
+    public Subscribe 구독하기(Subscribe subscribe) {
+        subscribeDao.insert(subscribe);// id, usersId, boardsId 완벽하게 동기화 할려면 SELECT를 해서 해야한다,
+        return subscribe;
+    }
+
+    public void 구독취소(Integer subscribeId) {
+        subscribeDao.deleteById(subscribeId);
+    }
+
 }
